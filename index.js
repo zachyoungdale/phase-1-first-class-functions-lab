72bd8e006bbdd6bb5719b891ffbf8bf23ced61d0
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-30 14:14:37 -0400

    commit tree 476b1396c42809d99e4cdb142e32f999f08713fb

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+
+const returnFirstTwoDrivers = (drivers) => {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    return drivers.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier =  (fare) => {
+    return function (integer) {
+        return fare * integer;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (drivers, selection) => {
+    if (selection === returnFirstTwoDrivers) {
+        return drivers.slice(0,2);
+    } else if (selection === returnLastTwoDrivers) {
+        return drivers.slice(2,4);
+    }
+}
